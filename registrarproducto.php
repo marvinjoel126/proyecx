3555d3c79fe250bb7a504be6476167209d691e4c
Marvin <marvinchen125mail.com>
2021-06-12 12:19:34 -0600

    cuarto commit :V

---
+++ registrarproducto.php
@@ -0,0 +1,26 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "proyec";
+
+$conn = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+if(!$conn){
+    die("Conexion Fallida: ".mysqli_connect_error());
+}else{
+   
+
+$producto = $_POST['producto'];
+$cantidad = $_POST['cantidad'];
+$precio = $_POST['precio'];
+$total = $_POST['total'];
+   
+        $sql = "INSERT INTO registro (producto, cantidad, precio,total) VALUE ('".$producto."','".$cantidad."','".$precio."','".$total."')";
+        if(mysqli_query($conn,$sql)){
+            header("Location:inicio.html");
+        }else{
+            echo "ERROR AL REGISTRARSE";
+        }
+    
+}
+?>
